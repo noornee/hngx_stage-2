c1264044a6745459422919950f98bff67f8cb447
noornee <noornee.011@gmail.com>
2023-09-12 08:51:55 +0100

    chore: added a route for a 404 page

--- pkg/controller/user/user.go
+++ pkg/controller/user/user.go
@@ -13,22 +13,22 @@ func (base *Controller) CreateUser(c *gin.Context) {
 	req := models.CreateUserRequest{}
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "failed to parse request", "error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "failed to parse request", "error": err.Error(), "status": http.StatusBadRequest})
 		return
 	}
 
 	if err := base.Validator.Struct(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "validation failed", "error": utility.ValidationResponse(err, base.Validator)})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "validation failed", "error": utility.ValidationResponse(err, base.Validator), "status": http.StatusBadRequest})
 		return
 	}
 
 	code, err := services.CreateUserService(base.DB, req)
 	if err != nil {
-		c.JSON(code, gin.H{"message": "failed to parse request", "error": err.Error()})
+		c.JSON(code, gin.H{"message": "failed to parse request", "error": err.Error(), "status": code})
 		return
 	}
 
-	c.JSON(code, gin.H{"message": "user creation successful"})
+	c.JSON(code, gin.H{"message": "user creation successful", "status": code})
 }
 
 func (base *Controller) GetUser(c *gin.Context) {
@@ -36,11 +36,11 @@ func (base *Controller) GetUser(c *gin.Context) {
 
 	result, code, err := services.GetUserService(base.DB, username)
 	if err != nil {
-		c.JSON(code, gin.H{"message": "failed to parse request", "error": err.Error()})
+		c.JSON(code, gin.H{"message": "failed to parse request", "error": err.Error(), "status": code})
 		return
 	}
 
-	c.JSON(code, gin.H{"data": result})
+	c.JSON(code, gin.H{"message": "success", "data": result, "status": code})
 }
 
 func (base *Controller) UpdateUser(c *gin.Context) {
@@ -49,17 +49,17 @@ func (base *Controller) UpdateUser(c *gin.Context) {
 	id := c.Params.ByName("id")
 
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "failed to parse request", "error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "failed to parse request", "error": err.Error(), "status": http.StatusBadRequest})
 		return
 	}
 
 	code, err := services.UpdateUserService(base.DB, req, id)
 	if err != nil {
-		c.JSON(code, gin.H{"message": "failed to parse request", "error": err.Error()})
+		c.JSON(code, gin.H{"message": "failed to parse request", "error": err.Error(), "status": code})
 		return
 	}
 
-	c.JSON(code, gin.H{"message": "user update successful"})
+	c.JSON(code, gin.H{"message": "user update successful", "status": code})
 }
 
 func (base *Controller) DeleteUser(c *gin.Context) {
@@ -67,9 +67,9 @@ func (base *Controller) DeleteUser(c *gin.Context) {
 
 	code, err := services.DeleteUserService(base.DB, username)
 	if err != nil {
-		c.JSON(code, gin.H{"message": "failed to parse request", "error": err.Error()})
+		c.JSON(code, gin.H{"message": "failed to parse request", "error": err.Error(), "status": code})
 		return
 	}
 
-	c.JSON(code, gin.H{"message": "user delete successful"})
+	c.JSON(code, gin.H{"message": "user delete successful", "status": code})
 }
--- pkg/router/router.go
+++ pkg/router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"github.com/noornee/hngx_stage-2/pkg/repository/storage/mongodb"
@@ -14,6 +16,13 @@ func Setup(db *mongodb.Database, validator *validator.Validate) *gin.Engine {
 
 	// User route
 	User(r, db, validator)
+
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "Page not found.",
+			"status":  http.StatusNotFound,
+		})
+	})
 
 	return r
 }
